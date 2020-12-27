e855466292b7bf24a94f9d692d7c5abcf64100b8
enesbytmr <enesbytmr@gmail.com>
2020-12-27 19:01:30 +0300

    modified soru's

--- soru 13.c
+++ soru 13.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
-int main(){
-    float x,y;
-     printf("Bu program ekteki dosya icerisindeki sekle gore hazirlanmistir.");
+int main() {
+    float x, y;
+    printf("Bu program ekteki dosya icerisindeki sekle gore hazirlanmistir.");
     printf("Koordinatlari giriniz");
-    scanf("%f %f",&x,&y);
-    if(){
-
-    }else{
-
-    }
+    scanf("%f %f", & x, & y);
+    if (abs(x) <= 1 && abs(y) <= 1 && abs(x) + abs(y) > 1) {
+        printf("Verdiginiz Koordinatlar alanin icindedir.");
+    } else {
+        printf("Verdiginiz Koordinatlar alanin disindadir.");
+    };
     return 0;
 }
--- soru3.c
+++ soru3.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
-int main(){
-    float x,y;
-      printf("Bu program ekteki dosya icerisindeki sekle gore hazirlanmistir.");
-    printf("Koordinatlari giriniz");
-    scanf("%f %f",&x,&y);
-    if( y >= 0 && y <= -pow(x, 2) + 2){
-       printf("Verdiginiz Koordinatlar alanin icindedir.");
-    }else{
-        printf("Verdiginiz Koordinatlar alanin disindadir.");
-    };
-    return 0;
+int main() {
+  float x, y;
+  printf("Bu program ekteki dosya icerisindeki sekle gore hazirlanmistir.");
+  printf("Koordinatlari giriniz");
+  scanf("%f %f", & x, & y);
+  if (y>=0 && y<= -x*x +2) {
+    printf("Verdiginiz Koordinatlar alanin icindedir.");
+  } else {
+    printf("Verdiginiz Koordinatlar alanin disindadir.");
+  };
+  return 0;
 }
